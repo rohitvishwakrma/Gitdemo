07e8aeaba2e7ff3405d0b32785d72008f65409f3
rohitvishwakrma <rohitvishwakarma02198@gamil.com>
2024-10-22 13:16:44 +0530

    third commit

--- Inheritance.java
+++ Inheritance.java
@@ -1,31 +1,36 @@
 /**
  * InnerInheritance
  */
- class  parent {
+class parent {
     String name;
-    String gender;    
-    void dis(String name,String gender){
+    String gender;
+
+    void dis(String name, String gender) {
         System.out.println("parent name:" + name);
-        System.out.println("parent gender::"+ gender);
+        System.out.println("parent gender::" + gender);
     }
-}
-class son extends parent{
-    int age ;
-    String Sname;
-    void dis(){
-        System.out.println("the son name is:"+ Sname);
-        System.out.println("son age is :"+ age);
-    }
- void disp(){
-  System.out.println(" here pull cmd"):
 }
 
-public class Inheritance {
-public static void main(String[] args) {
-    son s=new son( );
-     s.Sname="krtika";
-     s.age=34;
-     s.dis();
-     s.dis("kanha", "male");
- }
+class son extends parent {
+    int age;
+    String Sname;
+
+    void dis() {
+        System.out.println("the son name is:" + Sname);
+        System.out.println("son age is :" + age);
+    }
+
+ void disp(){
+  System.out.println(" here pull cmd");
+}
+
+    public class Inheritance {
+        public static void main(String[] args) {
+            son s = new son();
+            s.Sname = "krtika";
+            s.age = 34;
+            s.dis();
+            s.dis("kanha", "male");
+        }
+    }
 }
