bd33aa24bfc9c1554cb6a5611ba07273ad95466d
rohitvishwakrma <rohitvishwakarma02198@gmail.com>
2024-10-22 13:18:57 +0530

    Update Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -16,6 +16,8 @@ class son extends parent{
         System.out.println("the son name is:"+ Sname);
         System.out.println("son age is :"+ age);
     }
+
+ // here i modified 
  void disp(){
   System.out.println(" here pull cmd"):
 }
